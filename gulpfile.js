97b6753d63cf0553e3b7501508fd751586cc2244
wangyan <promiseid@126.com>
2016-11-23 15:45:39 +0800

    sass-牛刀小试-弹窗

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	watch = require('gulp-watch'),
   	connect = require('gulp-connect');
+var sourcemaps = require('gulp-sourcemaps');
  
 gulp.task('connect', function() {
   connect.server({
@@ -18,5 +19,17 @@ gulp.task('listen',function(){
       .pipe(connect.reload());
   });
 });
- 
+
+gulp.task('way1',function () {
+	return watch('./way-1/sass/*.scss',function () {
+		gulp.src('./way-1/sass/*.scss')
+			//.pipe(sourcemaps.init())
+			.pipe(sass().on('error',sass.logError))
+			//.pipe(sourcemaps.write())
+			.pipe(gulp.dest('./way-1/css'))
+			.pipe(connect.reload());
+	});
+});
+
 gulp.task('default', ['connect','listen']);
+gulp.task('default-way1',['connect','way1']);
